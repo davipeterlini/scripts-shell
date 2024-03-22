9467f733b12f657ff40baba549fe5b0c71939b65
Davi Gonçalves Peterlini <davi.peterlini@MMB026308CPS.local>
2024-03-22 11:56:11 -0300

    adding new script: setup_iterm_dev.sh

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # scripts
+
+## setup_iterm_dev.sh
+* Reference - [Otimização do Terminal](https://medium.com/@arojunior/otimizando-o-terminal-do-mac-os-x-com-iterm-e-oh-my-zsh-1b0e843b5eb2)
+
+* Requirements
+```shell script
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew install --cask iterm2
+```
+
+* Exec scripts
+```shell script
+./mac/setup_iterm_dev.sh
+```
---
+++ mac/setup_iterm_dev.sh
@@ -0,0 +1,25 @@
+#!/bin/zsh
+
+# Config Oh My Zsh
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+else
+    echo "Oh My Zsh aready install"
+fi
+
+# Create a New Profile and set default
+# TODO - the line below is not work beacause this command not suported in iTerm2
+#osascript iterm/create_profile.scpt
+echo "Note: Create profile and set as default of this iTerm2 must be done manually."
+
+# Download Themas
+curl -o ~/Downloads/material-design-colors.itermcolors https://raw.githubusercontent.com/MartinSeeler/iterm2-material-design/master/material-design-colors.itermcolors
+#curl -o ~/Downloads/solarized.itermcolors https://raw.githubusercontent.com/altercation/solarized/master/iterm2-colors-solarized/Solarized%20Dark.itermcolors
+echo "Note: The import of this iTerm2 theme must be done manually."
+
+# Clona e instala as fontes Powerline
+git clone https://github.com/powerline/fonts.git && cd fonts && ./install.sh
+echo "Note: After installation, you need to manually set the font in iTerm2 to 'Meslo LG L for Powerline'."
+
+# Modifica o arquivo .zshrc para usar o tema 'agnoster'
+sed -i '' 's/ZSH_THEME="robbyrussell"/# ZSH_THEME="robbyrussell"\nZSH_THEME="agnoster"/' ~/.zshrc
