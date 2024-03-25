cccc2ffc76008383552c66e402e9bc8b60fb0aae
Davi Gonçalves Peterlini <davi.peterlini@MMB026308CPS.local>
2024-03-25 17:42:17 -0300

    fix script install_apps.sh - linux

--- linux/install_apps.sh
+++ linux/install_apps.sh
@@ -6,19 +6,22 @@ sudo apt update
 
 # Function to install software if it's not already installed
 install_if_not_installed() {
-    local name="$1"
+    local preparation="$1"
+    local name="$2"
 
     # Check if the software is already installed
     if dpkg -l | grep -qw "$name"; then
         echo "$name is already installed."
     else
         echo "Installing $name..."
+        $preparation
         sudo apt install -y $name
+
     fi
 }
 
 # Software to install (use APT package names)
-install_if_not_installed "ssh"
+install_if_not_installed "xclip"
 install_if_not_installed "git"
 install_if_not_installed "wget"
 install_if_not_installed "python3"
@@ -31,7 +34,8 @@ install_if_not_installed "intellij-idea-community"  # or 'intellij-idea-ultimate
 #install_if_not_installed "postman"   #
 install_if_not_installed "obs-studio"
 #install_if_not_installed "android-studio"
-#install_if_not_installed "spotify-client"
+install_if_not_installed "curl -sS https://download.spotify.com/debian/pubkey_6224F9941A8AA6D1.gpg | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
+echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list" "spotify-client"
 #install_if_not_installed "rancher-desktop"  # May need to install via other means if not available on APT
 #install_if_not_installed "google-drive-ocamlfuse"  # May need to configure after installation
 install_if_not_installed "virtualbox"
