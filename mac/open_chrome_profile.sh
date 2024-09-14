4f76cb0953c73c9b638e399d159822085e6700ab
Davi Gonçalves Peterlini <davi.peterlini@MMB026308CPS.local>
2024-09-14 12:23:01 -0300

    update scripts to linux

---
+++ mac/open_chrome_profile.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+
+# TODO: para encontrar o profile só procurar por "Profile" no path: cd ~/Library/Application\ Support/Google/Chrome
+# Function to check if an application is running
+is_app_running() {
+    local app_name="$1"
+    if pgrep -x "$app_name" > /dev/null; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+# Function to open Google Chrome with a specific profile
+open_chrome_with_profile() {
+    local profile="$1"
+
+    if is_app_running "Google Chrome"; then
+        echo "Google Chrome is already running."
+    else
+        echo "Opening Google Chrome with profile $profile..."
+        open -a "Google Chrome" --args --profile-directory="$profile"
+    fi
+}
+
+# Main function to open Google Chrome with the specified profile
+main() {
+    local profile="$1"
+
+    if [ -z "$profile" ]; then
+        echo "No profile specified. Exiting..."
+        exit 1
+    fi
+
+    open_chrome_with_profile "$profile"
+}
+
+# Execute the main function with the provided argument
+main "$1"
