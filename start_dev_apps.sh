a400ee1276a62fa4bea81fb2e7d9296ba741c545
Davi Gonçalves Peterlini <davi.peterlini@MMB026308CPS.local>
2024-09-14 11:59:33 -0300

    update scripts to linux

--- start_dev_apps.sh
+++ start_dev_apps.sh
@@ -34,24 +34,55 @@ start_apps_windows() {
     execute_script "./windows/open_terminal_tabs.bat"
 }
 
-# Main function to start development applications
-main() {
-    os_choice=$(choose_os)
-    echo "Operating system chosen: $os_choice"
+# TODO: corrigir para pegar do utils/choose_os.sh
+# Function to detect the operating system and execute the corresponding script
+detect_and_start_apps() {
+    echo "Detecting the operating system..."
 
-    case "$os_choice" in
-        macOS)
+    case "$(uname -s)" in
+        Darwin)
+            echo "macOS detected."
             start_apps_mac
             ;;
         Linux)
+            echo "Linux detected."
             start_apps_linux
             ;;
-        Windows)
+        CYGWIN*|MINGW32*|MSYS*|MINGW*)
+            echo "Windows detected."
             start_apps_windows
             ;;
         *)
             echo "Unsupported operating system."
             exit 1
+            ;;
+    esac
+}
+
+# Main function to start development applications
+main() {
+    echo "Choose the operating system to start development applications:"
+    echo "1) macOS"
+    echo "2) Linux"
+    echo "3) Windows"
+    read -p "Enter the number corresponding to your choice (or press Enter to auto-detect): " os_choice
+
+    case "$os_choice" in
+        1)
+            echo "macOS chosen."
+            start_apps_mac
+            ;;
+        2)
+            echo "Linux chosen."
+            start_apps_linux
+            ;;
+        3)
+            echo "Windows chosen."
+            start_apps_windows
+            ;;
+        *)
+            echo "No valid choice made. Auto-detecting the operating system..."
+            detect_and_start_apps
             ;;
     esac
 }
