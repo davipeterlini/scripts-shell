de9d4b0f9427fdc918de1f4124e26dc2678375fc
Davi Gonçalves Peterlini <davi.peterlini@MMB026308CPS.local>
2024-09-14 11:46:19 -0300

    update scripts to linux

--- windows/open_terminal_tabs.bat
+++ windows/open_terminal_tabs.bat
@@ -1,11 +1,7 @@
 @echo off
 
-:: List of directories to open in new terminal tabs
-set tabs=(
-    "C:\path\to\project1"
-    "C:\path\to\project2"
-    "C:\path\to\project3"
-)
+:: Load environment variables from .env file
+for /f "tokens=1,* delims==" %%a in ('type .env') do set %%a=%%b
 
 :: Function to open a new terminal tab
 :open_tab
@@ -14,7 +10,16 @@ echo Opening terminal tab for %tab_path%...
 start "" "cmd.exe" /k "cd /d %tab_path%"
 exit /b
 
+:: List of directories to open in new terminal tabs
+setlocal enabledelayedexpansion
+set tabs=(
+    %ITERM_OPEN_TABS_WORK_1%
+    %ITERM_OPEN_TABS_WORK_2%
+    %ITERM_OPEN_TABS_WORK_3%
+)
+
 :: Loop through the list of directories and open each one in a new terminal tab
-for %%a in %tabs% do (
+for %%a in (!tabs!) do (
     call :open_tab "%%a"
 )
+endlocal
