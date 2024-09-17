ce46edee612f474a85438fea11224ea603738e68
Davi Gonçalves Peterlini <davi.peterlini@MMB026308CPS.local>
2024-09-17 13:13:10 -0300

    fix: start_dev

--- utils/load_env.sh
+++ utils/load_env.sh
@@ -3,20 +3,34 @@
 # Get the directory of the current script
 SCRIPT_DIR=$(dirname "$(realpath "$0")")
 
+# Function to find the .env file in the project root directory
+find_env_file() {
+  local dir="$SCRIPT_DIR"
+  while [ "$dir" != "/" ]; do
+    if [ -f "$dir/.env" ]; then
+      echo "$dir/.env"
+      return
+    fi
+    dir=$(dirname "$dir")
+  done
+  echo ""
+}
+
 # Function to load environment variables from .env and .env.local files
 load_env() {
-  if [ -f "$SCRIPT_DIR/../.env" ]; then
+  local env_file=$(find_env_file)
+  if [ -f "$env_file" ]; then
     set -a
-    source "$SCRIPT_DIR/../.env"
+    source "$env_file"
     set +a
   else
     echo ".env file not found. Exiting..."
     exit 1
   fi
 
-  if [ -f "$SCRIPT_DIR/../.env.local" ]; then
+  if [ -f "$env_file.local" ]; then
     set -a
-    source "$SCRIPT_DIR/../.env.local"
+    source "$env_file.local"
     set +a
   else
     echo ".env.local file not found. Make sure to create it for sensitive information."
@@ -26,16 +40,17 @@ load_env() {
 # Function to load a specific environment variable from .env and .env.local files
 load_env_var() {
   local var_name="$1"
-  if [ -f "$SCRIPT_DIR/../.env" ]; then
-    local var_value=$(grep -v '^#' "$SCRIPT_DIR/../.env" | grep -E "^${var_name}=" | cut -d '=' -f2-)
+  local env_file=$(find_env_file)
+  if [ -f "$env_file" ]; then
+    local var_value=$(grep -v '^#' "$env_file" | grep -E "^${var_name}=" | cut -d '=' -f2-)
     export "${var_name}=${var_value}"
   else
     echo ".env file not found. Exiting..."
     exit 1
   fi
 
-  if [ -f "$SCRIPT_DIR/../.env.local" ]; then
-    local var_value=$(grep -v '^#' "$SCRIPT_DIR/../.env.local" | grep -E "^${var_name}=" | cut -d '=' -f2-)
+  if [ -f "$env_file.local" ]; then
+    local var_value=$(grep -v '^#' "$env_file.local" | grep -E "^${var_name}=" | cut -d '=' -f2-)
     export "${var_name}=${var_value}"
   else
     echo ".env.local file not found. Make sure to create it for sensitive information."
