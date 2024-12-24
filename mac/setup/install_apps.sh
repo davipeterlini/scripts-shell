9602212bf7d2d902f1e86fdd05b0827f3dc8ae1e
davipeterlini@gmail.com <davi.peterlini@ciandt.com>
2024-12-24 12:42:45 -0300

    modified linux scripts

--- mac/setup/install_apps.sh
+++ mac/setup/install_apps.sh
@@ -43,7 +43,7 @@ install_all_apps() {
 
 main() {
     # Load environment variables
-    source "$(dirname "$0")/../utils/load_env.sh"
+    source "$(dirname "$0")/../../utils/load_env.sh"
     load_env
 
     # Check if dialog is installed
