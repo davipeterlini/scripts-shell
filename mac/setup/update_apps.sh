12a6428e82fcdb5e898de503b83f44bf5a969ddb
davipeterlini@gmail.com <davi.peterlini@ciandt.com>
2025-01-26 17:41:07 -0300

    fix setup for mac

--- mac/setup/update_apps.sh
+++ mac/setup/update_apps.sh
@@ -3,29 +3,23 @@
 # Load environment variables and utility functions
 source "$(dirname "$0")/../../utils/load_env.sh"
 load_env
-source "$(dirname "$0")/../../utils/display_menu.sh"
 source "$(dirname "$0")/install_homebrew.sh"
 
-# Function to update apps on macOS
-update_apps_mac() {
-    local apps=("$@")
-    for app in "${apps[@]}"; do
-        brew upgrade "$app"
-    done
+# Function to update all Homebrew packages
+update_all_apps_mac() {
+    echo "Updating Homebrew and all installed packages..."
+    brew update
+    brew upgrade
+    brew cleanup
 }
 
-# Main function to handle app updates based on user choice
+# Main function to handle the update process
 main() {
     # Install Homebrew if not installed
     install_homebrew
 
-    # Display menu and get user choices
-    choices=$(display_menu)
-
-    # Update selected apps
-    [[ "$choices" == *"1"* ]] && update_apps_mac $(echo "$INSTALL_APPS_BASIC_MAC" | tr ',' ' ')
-    [[ "$choices" == *"2"* ]] && update_apps_mac $(echo "$INSTALL_APPS_DEV_MAC" | tr ',' ' ')
-    [[ "$choices" == *"3"* ]] && update_apps_mac $(echo "$APPS_TO_INSTALL_MAC" | tr ',' ' ')
+    # Update all Homebrew packages
+    update_all_apps_mac
 }
 
 main
