1b4c13b920e325f918338841f1d2cc639876e1b7
davipeterlini@gmail.com <davi.peterlini@ciandt.com>
2025-01-26 21:13:16 -0300

    fix setup for mac

--- utils/detect_profile.sh
+++ utils/detect_profile.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+# TODO - não está detectando o profile coerretamente
+
 # Function to detect the shell profile being used
 detect_profile() {
   if [ -n "$ZSH_VERSION" ]; then
-    echo "zsh"
+    echo "$HOME/.zshrc"
   elif [ -n "$BASH_VERSION" ]; then
-    echo "bash"
+    echo "$HOME/.bashrc"
+  elif [ -n "$FISH_VERSION" ]; then
+    echo "$HOME/.config/fish/config.fish"
   else
     echo "unknown"
   fi
