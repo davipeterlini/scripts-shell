f50f969cf86b1172db53c156683a355c4439a0c3
davipeterlinicit <davi.peterlini@ciandt.com>
2025-04-16 15:33:00 -0300

    feat: pip alias

--- utils/load_env.sh
+++ utils/load_env.sh
@@ -3,28 +3,29 @@
 # Get the directory of the current script
 SCRIPT_DIR=$(dirname "$(realpath "$0")")
 
-# Function to find the .env file in the project root directory
-find_env_file() {
+# Function to find the .env and .env.local files in the project root directory
+find_env_files() {
   local dir="$SCRIPT_DIR"
   while [ "$dir" != "/" ]; do
     if [ -f "$dir/.env" ]; then
-      echo "$dir/.env"
+      ENV_FILE="$dir/.env"
+      ENV_LOCAL_FILE="$dir/.env.local"
       return
     fi
     dir=$(dirname "$dir")
   done
-  echo ""
+  echo "Error: .env file not found in any parent directory."
+  exit 1
 }
 
 # Function to determine the operating system and set the HOME variable accordingly
 set_home_based_on_os() {
-  local user="$1"
   case "$(uname -s)" in
     Darwin)
-      export HOME="/Users/$user"
+      export HOME="/Users/$USER"
       ;;
     Linux)
-      export HOME="/home/$user"
+      export HOME="/home/$USER"
       ;;
     *)
       echo "Unsupported operating system."
@@ -33,65 +34,47 @@ set_home_based_on_os() {
   esac
 }
 
-update_env_file() {
-  local env_file=$(find_env_file)
-  if [ -f "$env_file" ]; then
-    # Remove existing HOME definition if it exists
-    sed -i '' -e '/^HOME=/d' "$env_file"
-    # Insert the new HOME definition at the beginning of the file (without quotes)
-    sed -i '' -e '1i\
-HOME='"$HOME" "$env_file"
+# Function to create or update the .env.local file with the HOME variable
+update_env_local_file() {
+  if [ ! -f "$ENV_LOCAL_FILE" ]; then
+    echo "HOME=$HOME" > "$ENV_LOCAL_FILE"
+    echo "Created .env.local file with HOME variable."
   else
-    echo ".env file not found. Exiting..."
-    exit 1
-  fi
-}
-
-# Function to check if HOME is already set in the .env file
-check_existing_home() {
-  local env_file=$(find_env_file)
-  if [ -f "$env_file" ]; then
-    existing_home=$(grep '^HOME=' "$env_file" | cut -d '=' -f2-)
-    if [ -n "$existing_home" ]; then
-      echo "Existing HOME found in .env: $existing_home"
-      read -p "Do you want to use the existing HOME? (y/n): " choice
-      if [ "$choice" == "y" ]; then
-        export HOME="$existing_home"
-        return 0
-      fi
+    if grep -q '^HOME=' "$ENV_LOCAL_FILE"; then
+      sed -i '' "s|^HOME=.*|HOME=$HOME|" "$ENV_LOCAL_FILE"
+      echo "Updated HOME variable in .env.local file."
+    else
+      echo "HOME=$HOME" >> "$ENV_LOCAL_FILE"
+      echo "Added HOME variable to .env.local file."
     fi
   fi
-  return 1
 }
 
 # Function to load environment variables from .env and .env.local files
 load_env() {
-  local user
-  local env_file=$(find_env_file)
-  if [ -f "$env_file" ]; then
+  find_env_files
+
+  if [ -f "$ENV_FILE" ]; then
     set -a
-    source "$env_file"
+    source "$ENV_FILE"
     set +a
   else
-    echo ".env file not found. Exiting..."
+    echo "Error: .env file not found. Exiting..."
     exit 1
   fi
 
-  if [ -f "$env_file.local" ]; then
-    set -a
-    source "$env_file.local"
-    set +a
-  else
-    echo ".env.local file not found. Make sure to create it for sensitive information."
+  if [ ! -f "$ENV_LOCAL_FILE" ]; then
+    touch "$ENV_LOCAL_FILE"
+    echo "Created empty .env.local file."
   fi
 
-  # Check if HOME is already set and ask the user if they want to use it
-  if ! check_existing_home; then
-    # Prompt for user and set the HOME variable based on the operating system
-    read -p "Enter the USER for the environment: " user
-    set_home_based_on_os "$user"
-    # Update the .env file with the new HOME variable
-    update_env_file
+  set -a
+  source "$ENV_LOCAL_FILE"
+  set +a
+
+  if [ -z "$HOME" ]; then
+    set_home_based_on_os
+    update_env_local_file
   fi
 
   # Mark environment as loaded
