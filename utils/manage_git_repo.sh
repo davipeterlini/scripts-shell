efebaf1b08c96bbcfe2df42ef6c378c8d5306c06
davipeterlini <davipeterlini@gmail.com>
2025-06-03 12:48:14 -0300

    feat: fix setup for git with ssh key

--- utils/manage_git_repo.sh
+++ utils/manage_git_repo.sh
@@ -1,6 +1,32 @@
 #!/bin/bash
 
-# Function to manage repositories
+# Function to clone a repository
+clone_repository() {
+    local repo_url="$1"
+    local repo_path="$2"
+
+    print_info "Cloning repository: $(basename "$repo_url" .git)"
+    git clone "$repo_url" "$repo_path"
+}
+
+# Function to update a repository
+update_repository() {
+    local repo_path="$1"
+
+    print_info "Updating repository: $(basename "$repo_path")"
+    (cd "$repo_path" && git pull origin main)
+}
+
+# Function to merge back changes from a branch
+merge_back_repository() {
+    local repo_path="$1"
+    local branch="$2"
+
+    print_info "Merging back changes from branch $branch in repository: $(basename "$repo_path")"
+    (cd "$repo_path" && git merge "$branch")
+}
+
+# Function to manage repositories - Update or Clone repo
 manage_repositories() {
     declare -A repositories=("$@")
     for target_dir in "${!repositories[@]}"; do
@@ -9,11 +35,9 @@ manage_repositories() {
         local repo_path="$target_dir/$repo_name"
 
         if [[ -d "$repo_path" ]]; then
-            print_info "Updating repository: $repo_name"
-            (cd "$repo_path" && git pull)
+            update_repository "$repo_path"
         else
-            print_info "Cloning repository: $repo_name"
-            git clone "$repo_url" "$repo_path"
+            clone_repository "$repo_url" "$repo_path"
         fi
     done
 }
