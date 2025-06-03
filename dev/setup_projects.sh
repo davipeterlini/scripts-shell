9937dca6a8b3c2e6390fe49d4f3bfb2c1dfe5f08
davipeterlini <davipeterlini@gmail.com>
2025-06-03 19:57:14 -0300

    feat: fix setup scripts

--- dev/setup_projects.sh
+++ dev/setup_projects.sh
@@ -1,72 +1,62 @@
 #!/bin/bash
-# setup_work_projects.sh
-# Purpose: Set up and maintain work project repositories
+# setup_projects.sh
+# Purpose: Set up and maintain project repositories
 
 # Constants
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-PROJECT_ROOT="$(cd "$PROJECT_ROOT" && pwd)"
-source "$PROJECT_ROOT/utils/colors_message.sh"
-
-
-# Import utilities
+PROJECT_ROOT="$(cd "$SCRIPT_DIR/.." && pwd)"
 source "$PROJECT_ROOT/utils/colors_message.sh"
 source "$PROJECT_ROOT/utils/load_env.sh"
 source "$PROJECT_ROOT/utils/bash_tools.sh"
 source "$PROJECT_ROOT/utils/manage_git_repo.sh"
-source "./dev/utils/load_dev_env.sh"
 
-# Load environment variables
-load_env
-
-# Function to validate environment variables
-validate_env_variables() {
-    local project_type="$1"
-
-    if [[ "$project_type" == "work" ]]; then
-        if [[ -z "$PROJECT_WORK_DIR" || -z "$PROJECT_WORK_REPOS" ]]; then
-            print_error "Work project environment variables are not set. Please check your configuration."
-            exit 1
+# Function to display environment options and let the user choose
+select_environment() {
+    env_dir="$PROJECT_ROOT/dev/assets"
+    env_files=("$env_dir/.env.personal" "$env_dir/.env.work")
+    echo "Select an environment:"
+    select env_file in "${env_files[@]}"; do
+        if [ -n "$env_file" ]; then
+            echo "You selected $env_file"
+            echo "$env_file"
+            return 
+        else
+            echo "Invalid selection. Try again."
         fi
-        PROJECT_DIRS=$PROJECT_WORK_DIR
-        PROJECT_REPOS=$PROJECT_WORK_REPOS
-    elif [[ "$project_type" == "personal" ]]; then
-        if [[ -z "$PROJECT_PERSONAL_DIR" || -z "$PROJECT_PERSONAL_REPOS" ]]; then
-            print_error "Personal project environment variables are not set. Please check your configuration."
-            exit 1
-        fi
-        PROJECT_DIRS=$PROJECT_PERSONAL_DIR
-        PROJECT_REPOS=$PROJECT_PERSONAL_REPOS
-    else
-        print_error "Invalid project type: $project_type"
-        exit 1
-    fi
+    done
 }
 
 # Main script execution
 setup_projects_main() {
-    # Get user's choice between personal and work projects
-    local project_type="$1"
-    local project_name="$2"
-    local asset_file="$3"
+    # Select environment
+    select_environment
+    selected_env=$env_file
 
-    #print_info "Selected project type: $project_type"
+    echo $env_file
 
-    # Load environment variables based on user's choice
-    load_dev_env
-
-    # If project_name and asset_file are provided, select environment based on project
-    if [[ -n "$project_name" && -n "$asset_file" ]]; then
-        select_env_based_on_project "$project_name" "$asset_file"
+    # Load the selected environment variables
+    if [ -f "$selected_env" ]; then
+        set -a
+        source "$selected_env"
+        set +a
+    else
+        print_error "Environment file not found: $selected_env"
+        exit 1
     fi
 
-    # Validate environment variables
-    validate_env_variables "$project_type"
+    # Validate required variables
+    if [ -z "$PROJECT_DIR" ] || [ -z "$PROJECT_REPOS" ]; then
+        print_error "PROJECT_DIR or PROJECT_REPOS is not defined in the selected .env file. Exiting."
+        exit 1
+    fi
 
-    # Create directories and manage repositories
-    create_directories "${!PROJECT_DIRS[@]}"
+    # Create directories
+    create_directories "${PROJECT_DIR[@]}"
+
+    # Clone or update repositories
     manage_repositories "${PROJECT_REPOS[@]}"
 
-    print_success "${project_type^} projects setup completed successfully!"
+    print_success "Project setup completed successfully!"
 }
 
 # Check if the script is being executed directly or sourced
