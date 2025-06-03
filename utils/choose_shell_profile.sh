52669c537348fa613bc2d66cd78826b3fc2fb630
davipeterlini <davipeterlini@gmail.com>
2025-06-03 12:52:11 -0300

    feat: fix setup for git with ssh key

--- utils/choose_shell_profile.sh
+++ utils/choose_shell_profile.sh
@@ -1,25 +1,28 @@
 #!/bin/bash
 
+source "$(dirname "$0")/../../utils/colors_message.sh"
+
 # Function to ask the user which shell they are using and set the profile file
 choose_shell_profile() {
-  echo "Which shell are you using? (Enter the corresponding number)"
-  echo "1) bash"
-  echo "2) zsh"
+  print_header "Which shell are you using? (Enter the corresponding number)"
+  print_info "1) bash"
+  print_info "2) zsh"
   read -p "Choose an option (1 or 2): " shell_choice
 
   case $shell_choice in
     1)
       profile_file="$HOME/.bashrc"
+      print_success "Profile file set to $profile_file"
       ;;
     2)
       profile_file="$HOME/.zshrc"
+      print_success "Profile file set to $profile_file"
       ;;
     *)
-      echo "Invalid option. Exiting..."
+      print_error "Invalid option. Exiting..."
       exit 1
       ;;
   esac
 
-  echo "Profile file set to $profile_file"
   export PROFILE_FILE="$profile_file"
 }
--- utils/detect_os.sh
+++ utils/detect_os.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 
+source "$(dirname "$0")/../../utils/colors_message.sh"
+
 # Function to detect the operating system
 detect_os() {
+    print_info "Detecting the operating system..."
     case "$(uname -s)" in
         Darwin)
+            print_success "Operating System detected: macOS"
             echo "macOS"
             ;;
         Linux)
+            print_success "Operating System detected: Linux"
             echo "Linux"
             ;;
         CYGWIN*|MINGW32*|MSYS*|MINGW*)
+            print_success "Operating System detected: Windows"
             echo "Windows"
             ;;
         *)
-            echo "Unsupported OS"
+            print_error "Unsupported Operating System"
             exit 1
             ;;
     esac
