ef36608422a972840ff69f3492f7b925e5c3c923
davipeterlini <davipeterlini@gmail.com>
2025-06-03 14:33:19 -0300

    feat: fix setup scripts

--- dev/project-folder/setup_work_projects.sh
+++ dev/project-folder/setup_work_projects.sh
@@ -11,25 +11,28 @@ source "$(dirname "$0")/../../utils/colors_message.sh"
 source "$(dirname "$0")/../../utils/load_env.sh"
 source "$(dirname "$0")/../../utils/bash_tools.sh"
 source "$(dirname "$0")/../../utils/manage_git_repo.sh"
+source "$(dirname "$0")/../../utils/list_projects.sh"
 
 # Load environment variables
 load_env
 
 # Main script execution
 main() {
-    if [[ -z "$PROJECT_DIR_WORK" ]]; then
-        print_error "PROJECT_DIR_WORK environment variable is not set. Please check your root .env file."
+    list_projects
+
+    if [[ -z "$PROJECT_WORK_DIR" ]]; then
+        print_error "PROJECT_WORK_DIR environment variable is not set. Please check your root .env file."
         exit 1
     fi
 
-    if [[ -z "$REPOSITORIES_WORK" ]]; then
-        print_error "REPOSITORIES_WORK environment variable is not set. Please check your root .env file."
+    if [[ -z "$PROJECT_WORK_REPOS" ]]; then
+        print_error "PROJECT_WORK_REPOS environment variable is not set. Please check your root .env file."
         exit 1
     fi
 
-    # Convert REPOSITORIES_WORK from a comma-separated string to an associative array
+    # Convert PROJECT_WORK_REPOS from a comma-separated string to an associative array
     declare -A REPOSITORIES
-    IFS=',' read -ra REPO_PAIRS <<< "$REPOSITORIES_WORK"
+    IFS=',' read -ra REPO_PAIRS <<< "$PROJECT_WORK_REPOS"
     for pair in "${REPO_PAIRS[@]}"; do
         IFS='=' read -r dir repo <<< "$pair"
         REPOSITORIES["$dir"]="$repo"
--- utils/list_projects.sh
+++ utils/list_projects.sh
@@ -6,10 +6,10 @@ source "$SCRIPT_DIR/colors_message.sh"
 
 # Function to display available projects
 list_projects() {
-  print_header "Available Projects:"
+  print_info "Available Projects:"
   local index=1
   for identity in $(env | grep '^PROJECT_DIR_' | sed 's/^PROJECT_DIR_//' | sed 's/=.*//'); do
-    print_info "  $index) $(echo $identity | tr '[:upper:]' '[:lower:]')"  # Convert to lowercase
+    print "$index) $(echo $identity | tr '[:upper:]' '[:lower:]')"  # Convert to lowercase
     index=$((index + 1))
   done
 }
