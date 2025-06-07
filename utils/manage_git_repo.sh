c645e60c3d5c630df00c782833e3dd8b8e73fdf5
davipeterlini <davipeterlini@gmail.com>
2025-06-07 15:42:04 -0300

    feat: remove journey beacause sync with drive

--- utils/manage_git_repo.sh
+++ utils/manage_git_repo.sh
@@ -6,14 +6,17 @@ source "$SCRIPT_DIR/colors_message.sh"
 
 # Function to clone a repository
 clone_repository() {
-    local repo_path="$1"
-    local repo_url="$2"
+    local repo_url="$1"
+    local repo_path="$2"
+
+    print_header "git clone $repo_url $repo_path"
 
     print_info "Cloning repository: $(basename "$repo_url" .git)"
-    git clone "$repo_url" "$repo_path"
+    #git clone "$repo_url" "$repo_path"
 }
 
 # Function to update a repository
+# TODO - o nome do path precisa estar completo com o nome da pasta do repo
 update_repository() {
     local repo_path="$1"
 
@@ -31,13 +34,14 @@ merge_back_repository() {
 }
 
 # Function to manage repositories - Update or Clone repo
-# Modified to work with Bash 3.2 (no associative arrays)
 manage_repositories() {
     # Process arguments in pairs (target_dir and repo_url)
     while [[ $# -ge 2 ]]; do
-        local target_dir="$1"
-        local repo_url="$2"
+        local repo_url="$1"
+        local target_dir="$2"
         shift 2
+
+        print_header $target_dir
         
         local repo_name=$(basename "$repo_url" .git)
         local repo_path="$target_dir/$repo_name"
@@ -49,3 +53,11 @@ manage_repositories() {
         fi
     done
 }
+
+# Main function
+main() {
+    manage_repositories "$@"
+}
+
+# Execute main function
+main "$@"
