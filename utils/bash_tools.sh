f3cd23c3e6dae277aab63db5bcc9b5638e66dc40
davipeterlini <davipeterlini@gmail.com>
2025-06-08 15:55:16 -0300

    feat: fix setup_project.sh scripts

--- utils/bash_tools.sh
+++ utils/bash_tools.sh
@@ -5,18 +5,22 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source "$SCRIPT_DIR/colors_message.sh"
 
 # Function to create directories
+# Function to create directories
 create_directories() {
-    local ROOT_DIR="$1"
-    local directories=("$@")
-    for dir in "${directories[@]}"; do
-        full_dir="${ROOT_DIR}/${dir}"
-        print_header $full_dir
-        if [[ ! -d "$full_dir" ]]; then
-            print_info "Creating directory: $full_dir"
-            mkdir -p "$full_dir"
-            print_success "Directory created: $full_dir"
-        else
-            print_info "Directory already exists: $full_dir"
-        fi
-    done
+   local ROOT_DIR="$1"
+   local directories=("$@")
+   for dir in "${directories[@]}"; do
+       if [[ "$dir" != "$ROOT_DIR" ]]; then
+           print_header $dir
+           full_dir="${ROOT_DIR}/${dir}"
+           print_header $full_dir
+           if [[ ! -d "$full_dir" ]]; then
+               print_info "Creating directory: $full_dir"
+               mkdir -p "$full_dir"
+               print_success "Directory created: $full_dir"
+           else
+               print_info "Directory already exists: $full_dir"
+           fi
+       fi
+   done
 }
